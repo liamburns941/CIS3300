3c0afbbdda90177a59d5e62ba5344abbec4e34a9
liamburns941 <liamburns94@gmail.com>
2018-01-31 23:02:54 +0000

    added ignoredYellowBox

--- manager/index.js
+++ manager/index.js
@@ -1,3 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 AppRegistry.registerComponent('manager', () => App);
+
+console.ignoredYellowBox = [
+'Setting a timer'
+];
